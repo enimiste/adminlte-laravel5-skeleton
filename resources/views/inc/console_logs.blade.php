e1af6ff84b9db910e14285321caf669b6278296f
El bachir NOUNI <nouni.elbachir@gmail.com>
2017-03-17 16:52:11 +0000

    show done by user in logs

--- resources/views/inc/console_logs.blade.php
+++ resources/views/inc/console_logs.blade.php
@@ -7,14 +7,16 @@
         <table class="table table-hover">
             <tbody>
             <tr>
-                <th style="text-align: center;">Type</th>
+                <th style="text-align: center;min-width: 100px;">Type</th>
                 <th style="text-align: center;">Message</th>
-                <th style="text-align: center;">Traçé le</th>
+                <th style="text-align: center;min-width: 150px;">User</th>
+                <th style="text-align: center;min-width: 150px;">Traçé le</th>
             </tr>
             @foreach($console_logs as $clog)
                 <tr>
                     <td style="text-align: center;">{{ strtoupper($clog->type) }}</td>
                     <td>{{ $clog->message }}</td>
+                    <td>{{ $clog->by_user }}</td>
                     <td>{{ show_date_time($clog->created_at) }}</td>
                 </tr>
             @endforeach
