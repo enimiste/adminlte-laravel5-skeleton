1a5bfc8d8697477d96823e0a31a06f07ba827093
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2017-04-30 21:42:51 +0100

    Add JWTAuthenticateApi middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Org\Asso\Http\Middleware\JWTAuthenticateApi;
 
 class Kernel extends HttpKernel
 {
@@ -52,5 +53,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'nickel.jwt.auth' => JWTAuthenticateApi::class,
     ];
 }
---
+++ app/Http/Middleware/JWTAuthenticateApi.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elbachirnouni
+ * Date: 08/02/2017
+ * Time: 20:34
+ */
+
+namespace Org\Asso\Http\Middleware;
+
+
+use Closure;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Http\JsonResponse;
+use Org\Asso\Model\Events\HistoriqueDemandeAnnulationParticip;
+use Org\Asso\User;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\JWTAuth;
+
+class JWTAuthenticateApi
+{
+    /**
+     * @var JWTAuth
+     */
+    private $auth;
+
+    /**
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * InteractionByAdhesionAccount constructor.
+     * @param JWTAuth $auth
+     * @param Dispatcher $events
+     */
+    public function __construct(JWTAuth $auth, Dispatcher $events)
+    {
+        $this->auth = $auth;
+        $this->events = $events;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $uri = $request->getRequestUri();
+        if (env('ENABLE_AUTH', true)) {
+            if (!$token = $this->auth->setRequest($request)->getToken()) {
+                \Log::error('JWT Middleware : Token not provided : ' . $uri);
+                return $this->respond('notrasso.jwt.token_not_provided', 'Token not provided', 400);
+            }
+
+
+            try {
+                /** @var User $user */
+                $user = $this->auth->authenticate($token);
+            } catch (TokenExpiredException $e) {
+                \Log::error('JWT Middleware : ' . $e->getMessage() . ' : ' . $uri);
+                return $this->respond('notrasso.jwt.token_expired', 'Token expired', 403);
+            } catch (JWTException $e) {
+                \Log::error('JWT Middleware : ' . $e->getMessage() . ' : ' . $uri);
+                return $this->respond('notrasso.jwt.token_invalid', 'Token invalid', 403);
+            }
+
+            if (!$user) {
+                \Log::error('JWT Middleware : User not found : ' . $uri);
+                return $this->respond('tymon.jwt.user_not_found', 'User Not Found', 404);
+            }
+
+            $this->events->fire('tymon.jwt.valid', $user);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Fire event and return the response.
+     *
+     * @param  string $event
+     * @param  string $error
+     * @param  int $status
+     * @param  array $payload
+     * @return mixed
+     */
+    protected function respond($event, $error, $status, $payload = [])
+    {
+        $response = $this->events->fire($event, $payload, true);
+
+        return $response ?: new JsonResponse([
+            'auth_errors' => [$error]
+        ], $status);
+    }
+}
